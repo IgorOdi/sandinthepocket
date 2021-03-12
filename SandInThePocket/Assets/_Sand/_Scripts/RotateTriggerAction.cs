3fa2bfd545c47467697e73b58e0fe3f925db8449
franciscocid <franciscocidadehomemjunior@gmail.com>
2021-03-12 00:00:51 -0300

    Trigger & Weight buttons

---
+++ SandInThePocket/Assets/_Sand/_Scripts/RotateTriggerAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTriggerAction : MonoBehaviour, ITriggerableAction {
+	bool rotating;
+	float angle;
+
+	void Update() {
+		if (rotating) {
+			transform.eulerAngles = Vector3.up * angle;
+			angle += 50 * Time.deltaTime;
+		}
+	}
+
+	public void SetActive(bool active) {
+		rotating = active;
+	}
+}
---
+++ SandInThePocket/Assets/_Sand/_Scripts/WeightButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightButton : MonoBehaviour {
+
+	[SerializeField]
+	MonoBehaviour action;
+
+	[SerializeField]
+	Material activatedMaterial, deactivatedMaterial;
+
+	[SerializeField]
+	GameObject buttonChild;
+
+	bool active;
+
+	void OnTriggerEnter(Collider other) {
+		active = true;
+		((ITriggerableAction) action).SetActive (active);
+        buttonChild.GetComponent<MeshRenderer> ().material = activatedMaterial;
+        buttonChild.transform.localPosition = new Vector3 (0, -0.1f, 0);
+	}
+
+	void OnTriggerExit(Collider other) {
+		active = false;
+		((ITriggerableAction) action).SetActive (active);
+        buttonChild.GetComponent<MeshRenderer> ().material = deactivatedMaterial;
+        buttonChild.transform.localPosition = new Vector3 (0, 0, 0);
+	}
+}
