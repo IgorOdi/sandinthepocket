f6652611148af4301aaf6eb3145b4c22c495fbcb
Gabriel Escame <gabriel.escame@gmail.com>
2021-03-19 01:51:58 -0300

    feat: adding sequence puzzle

---
+++ SandInThePocket/Assets/_Sand/_Scripts/Puzzle/SequenceButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceButton : SwitchButton
+{
+    public Action<bool> onButtonPress;
+
+	public override void SetActive(bool active) {
+		this.active = active;
+		animator.SetBool ("Active", active);
+        onButtonPress?.Invoke (active);
+	}
+
+}
